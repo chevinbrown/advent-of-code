1a1a6bdcfd20e4491124b8b780eba792d9ae2502
Kevin Brown <chevinbrown@gmail.com>
2019-12-09 07:44:40 -0600

    Complete day 8

--- 2019/elixir/lib/day_08.ex
+++ 2019/elixir/lib/day_08.ex
@@ -12,16 +12,19 @@ defmodule Aoc.Day08 do
 
   def p1(input) do
     input
-    |> to_layers(@height, @width)
+    |> to_layers()
     |> layer_min_count(0)
     |> digit_multpl(1, 2)
   end
 
   def p2(input) do
+    input
+    |> to_layers()
+    |> flatten()
   end
 
-  def to_layers(pixels, h, w) do
-    pixels |> Enum.chunk_every(w * h)
+  def to_layers(pixels) do
+    pixels |> Enum.chunk_every(@width * @height)
   end
 
   def layer_min_count(layers, matching) do
@@ -31,4 +34,21 @@ defmodule Aoc.Day08 do
   def digit_multpl(layer, counta, countb) do
     Enum.count(layer, &(&1 == counta)) * Enum.count(layer, &(&1 == countb))
   end
+
+  def flatten(layers) do
+    layers
+    |> Enum.zip()
+    |> Enum.map(fn stack ->
+      stack
+      |> Tuple.to_list()
+      |> Enum.reject(&(&1 == 2))
+      |> hd
+      |> to_text()
+    end)
+    |> Enum.chunk_every(@width)
+    |> Enum.reduce("", &"#{&2}\n#{&1}")
+  end
+
+  def to_text(0), do: "◼️\s"
+  def to_text(1), do: "◻️\s"
 end
