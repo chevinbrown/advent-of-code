173ec0741952090f26ddeb2c5a77664fbdc5cfd5
Kevin Brown <chevinbrown@gmail.com>
2020-12-01 07:43:06 -0600

    Kick off AOC 2020

---
+++ 2020/elixir/lib/boilerplate.ex
@@ -0,0 +1,32 @@
+defmodule Aoc.Boilerplate do
+  defmacro __using__(_opts) do
+    quote do
+      def input_file(),
+        do: Path.join([Application.app_dir(:aoc, "priv"), "#{module_name()}.in"])
+
+      def read_input() do
+        input_file()
+        |> File.read!()
+      end
+
+      def stream_input() do
+        input_file()
+        |> File.stream!()
+      end
+
+      def integer_stream() do
+        stream_input()
+        |> Stream.map(&String.trim(&1))
+        |> Stream.map(&String.to_integer(&1))
+      end
+
+      defp module_name() do
+        __MODULE__
+        |> to_string
+        |> String.split(".")
+        |> List.last()
+        |> String.downcase()
+      end
+    end
+  end
+end
